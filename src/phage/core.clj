eaa627b3d9e91a360dcff9839934675ae1b20438
Stig Brautaset <stig@brautaset.org>
2014-08-21 00:21:51 +0100

    Initial test about frequencies of cells on board

--- src/phage/core.clj
+++ src/phage/core.clj
@@ -1,6 +1,38 @@
 (ns phage.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ^:private n-rows 8)
+(def ^:private n-columns 8)
+
+(defn idx
+  "Find index for row/column."
+  [row column]
+  (+ (* row n-columns) column))
+
+(def ^:private init-moves-left
+  (->> [:S :T :C :D :s :t :c :d]
+       (map (fn [x] [x 7]))
+       (into {})))
+
+(def ^:private init-cells 
+  (-> (repeat (* n-rows n-columns) nil)
+      (vec)
+      (assoc (idx 7 7) :D)
+      (assoc (idx 6 5) :T)
+      (assoc (idx 5 3) :S)
+      (assoc (idx 4 1) :C)
+      (assoc (idx 3 6) :c)
+      (assoc (idx 2 4) :s)
+      (assoc (idx 1 2) :t)      
+      (assoc (idx 0 0) :d)))
+
+(def start
+  "The starting state." 
+  (-> {}
+      (assoc :moves-left init-moves-left)
+      (assoc :cells init-cells)))
+
+
+                
+                       
+
+
--- test/phage/core_test.clj
+++ test/phage/core_test.clj
@@ -2,6 +2,9 @@
   (:require [clojure.test :refer :all]
             [phage.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest basics
+  (testing "starting cells"
+    (is (= (-> start (:cells) (frequencies)) {nil 56
+                                              :C 1 :S 1 :T 1 :D 1
+                                              :c 1 :s 1 :t 1 :d 1}))))
