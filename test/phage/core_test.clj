8b2b3448d50c267b464c3f8f4613e2f507a6a534
Stig Brautaset <stig@brautaset.org>
2014-08-22 00:29:04 +0100

    Add test to make sure unoccupied places return nil

--- test/phage/core_test.clj
+++ test/phage/core_test.clj
@@ -9,4 +9,5 @@
                                               :C 1 :S 1 :T 1 :D 1
                                               :c 1 :s 1 :t 1 :d 1}))
     (is (= (occupied? start 0 0) :d))
-    (is (= (occupied? start 7 7) :D))))
+    (is (= (occupied? start 7 7) :D))
+    (is (= (occupied? start 0 1) nil))))
