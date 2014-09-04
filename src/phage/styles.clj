b384e1472e1d17524ecdaaeff4114cad02ffe15c
Stig Brautaset <stig@brautaset.org>
2014-09-04 21:55:07 +0100

    Kill duplication in CSS generation

--- src/phage/styles.clj
+++ src/phage/styles.clj
@@ -2,58 +2,51 @@
   (:require [garden.def :refer [defstyles]]
             [garden.units :refer [px px-]]))
 
-(def pieces [[:.square1 :.square2
-              {:width (px 45)
-               :height (px 45)
-               :margin-left :auto
-               :margin-right :auto}]
+(defn square
+  [colour]
+  {:width (px 45)
+   :height (px 45)
+   :margin-left :auto
+   :margin-right :auto
+   :background colour})
 
-             [:.circle1 :.circle2
-              {:width (px 45)
-               :height (px 45)
-               :margin-left :auto
-               :margin-right :auto
-               :border-radius (px 50)}]
+(defn circle
+  [colour]
+  {:width (px 45)
+   :height (px 45)
+   :margin-left :auto
+   :margin-right :auto
+   :border-radius (px 50)
+   :background colour})
 
-             [:.triangle1
-              {:width 0
-               :height 0
-               :margin-left :auto
-               :margin-right :auto
-               :border-left [[(px 22.5) :solid :transparent]]
-               :border-right [[(px 22.5) :solid :transparent]]
-               :border-bottom [[(px 45) :solid :red]]
-               }]
+(defn diamond
+  [colour]
+  {:background colour
+   :width (px 33)
+   :height (px 33)
+   :margin-left :auto
+   :margin-right :auto
+   :-webkit-transform "rotate(45deg)"}  )
 
-             [:.triangle2
-              {:width 0
-               :height 0
-               :margin-left :auto
-               :margin-right :auto
-               :border-left [[(px 22.5) :solid :transparent]]
-               :border-right [[(px 22.5) :solid :transparent]]
-               :border-top [[(px 45) :solid :blue]]}]
+(defn triangle
+  [dir colour]
+  {:width 0
+   :height 0
+   :margin-left :auto
+   :margin-right :auto
+   :border-left [[(px 22.5) :solid :transparent]]
+   :border-right [[(px 22.5) :solid :transparent]]
+   (if (= dir :up) :border-bottom :border-top) [[(px 45) :solid colour]]})
 
-             [:.diamond1
-              {:background :red
-               :width (px 33)
-               :height (px 33)
-               :margin-left :auto
-               :margin-right :auto
-               :-webkit-transform "rotate(45deg)"}]
+(def pieces [[:.square1 (square :red)]
+             [:.circle1 (circle :red)]
+             [:.triangle1 (triangle :up :red)]
+             [:.diamond1 (diamond :red)]
 
-             [:.diamond2
-              {:background :blue
-               :width (px 33)
-               :height (px 33)
-               :margin-left :auto
-               :margin-right :auto
-               :-webkit-transform "rotate(45deg)"}]
-
-             [:.square1 :.circle1 {:background :red}]
-
-             [:.square2 :.circle2 {:background :blue}]])
-
+             [:.square2 (square :blue)]
+             [:.circle2 (circle :blue)]
+             [:.triangle2 (triangle :down :blue)]
+             [:.diamond2 (diamond :blue)]])
 
 (def cell [:.cell
            {:width (px 50)
