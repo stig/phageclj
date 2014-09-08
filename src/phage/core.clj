8cf0e8412602c9ddebc449f95486c6b9d9350cb1
Stig Brautaset <stig@brautaset.org>
2014-09-09 00:36:06 +0100

    Use a map rather than vector for the grid state
    - Keys are [x y] coordinates, values are keyword pieces (or nil)
    - Remove a couple unnecessary functions

--- src/phage/core.clj
+++ src/phage/core.clj
@@ -8,8 +8,18 @@
        (map (fn [x] [x 7]))
        (into {})))
 
-(def ^:private diamond-vectors #{8 1 -1 -8})
-(def ^:private square-vectors #{9 7 -7 -9})
+(def ^:private init-lookup
+  {:D [7 7]
+   :T [6 5]
+   :S [5 3]
+   :C [4 1]
+   :c [3 6]
+   :s [2 4]
+   :t [1 2]
+   :d [0 0]})
+
+(def ^:private diamond-vectors #{[1 0] [0 1] [0 -1] [-1 0]})
+(def ^:private square-vectors #{[1 1] [1 -1] [-1 1] [-1 -1]})
 (def ^:private circle-vectors (into square-vectors diamond-vectors))
 (def ^:private piece-vectors {:d diamond-vectors
                               :D diamond-vectors
@@ -17,87 +27,72 @@
                               :S square-vectors
                               :c circle-vectors
                               :C circle-vectors
-                              :t #{-1 1 8}
-                              :T #{-1 1 -8}})
+                              :t #{[1 0] [0 -1] [0 1]}
+                              :T #{[-1 0] [0 1] [0 -1]}})
 
-(defn idx
-  "Find index for row/column."
-  [row column] (+ (* row n-columns) column))
-
-(def ^:private init-cells
-  (-> (repeat (* n-rows n-columns) nil)
-      (vec)
-      (assoc (idx 7 7) :D)
-      (assoc (idx 6 5) :T)
-      (assoc (idx 5 3) :S)
-      (assoc (idx 4 1) :C)
-      (assoc (idx 3 6) :c)
-      (assoc (idx 2 4) :s)
-      (assoc (idx 1 2) :t)
-      (assoc (idx 0 0) :d)))
-
-(def start
-  "The starting state."
-  {:moves-left init-moves-left
-   :cells init-cells
-   :history []})
-
-(defn occupied?
-  "If the current position is occupied, return the piece."
-  ([state x] (get (:cells state) x)))
-
-(defn moves-left?
-  "Return the number of moves left for a piece, or nil if not a piece."
-  [state piece]
-  ((:moves-left state) piece))
 
 (def player-1-pieces #{:c :d :s :t})
 (def player-2-pieces #{:C :D :S :T})
 
+(def locations
+  (for [x (range 0 n-rows)
+        y (range 0 n-columns)]
+    [x y]))
+
+(def ^:private init-grid
+  (-> locations
+      (zipmap (repeat nil))
+      (merge (clojure.set/map-invert init-lookup))))
+
+(def start
+  "The starting state."
+  {:moves-left init-moves-left
+   :grid init-grid
+   :lookup init-lookup
+   :history []})
+
+(defn occupied?
+  "If the current position is occupied, return the piece."
+  [state coord]
+  (-> state :grid (get coord)))
+
+(defn moves-left?
+  "Return the number of moves left for a piece, or nil if not a piece."
+  [state piece]
+  (-> state :moves-left piece))
+
 (defn player-pieces
   [state]
-  (if (= 0 (rem (count (:history state)) 2))
+  (if (= 0 (-> state :history count (rem 2)))
     player-1-pieces
     player-2-pieces))
 
 (defn player-turn?
-  "Truthy if the piece specified belongs to current player."
+  "Truthy if the piece at this location belongs to current player."
   [state xy]
   (when-some [piece (occupied? state xy)]
     (piece (player-pieces state))))
 
-(defn- move-vector
-  "Finds vector from move."
-  [from to]
-  (letfn [(v [x] (cond
-                  (< x 0) -1
-                  (> x 0) 1
-                  :else 0))
-          (coord [idx]
-            [(quot idx n-columns) (rem idx n-columns)])]
-    (let [[x0 y0] (coord from)
-          [x1 y1] (coord to)]
-      (idx (v (- x1 x0)) (v (- y1 y0))))))
+(defn- add
+  "Add a move vector to a coordinate"
+  [[x y] [dx dy]]
+  [(+ x dx) (+ y dy)])
 
-(defn- clear-path?
-  "Checks whether the path from one location to another is free."
-  [state from to v]
-  (let [pos1 (+ from v)]
-    (when (contains? (:cells state) pos1)
-      (cond
-       (occupied? state pos1) false
-       (= to pos1) true
-       :else (recur state pos1 to v)))))
+(defn moves
+  "Return all legal moves from this state."
+  [state]
+  (for [p (player-pieces state)
+        v (piece-vectors p)
+        :when (< 0 (moves-left? state p))
+        :let [f ((:lookup state) p)]
+        t (rest (iterate (partial add v) f))
+        :while (and (contains? (:grid state) t) (not (occupied? state t)))]
+    [f t]))
 
 (defn legal-move?
   "Determines whether a move is legal."
-  [state [from to]]
-  (when (player-turn? state from)
-   (when-some [piece (occupied? state from)]
-     (when-some [mvs-left (moves-left? state piece)]
-       (when (< 0 mvs-left)
-         (when-some [v ((piece-vectors piece) (move-vector from to))]
-           (clear-path? state from to v)))))))
+  [state move]
+  (some #{move} (moves state)))
 
 (defn move
   "Perform a move. Returns the new state, or nil on error."
@@ -105,47 +100,28 @@
   (when (legal-move? state [from to])
     (let [piece (occupied? state from)]
       (-> state
+          (assoc-in [:lookup piece] to)
           (update-in [:history] #(conj % [from to]))
           (update-in [:moves-left piece] dec)
-          (assoc-in [:cells from] (if (piece player-1-pieces) :x :X))
-          (assoc-in [:cells to] piece)))))
-
-
-(defn- index-of [st x] (.indexOf (:cells st) x))
-
-(defn moves
-  "Return all legal moves from this state."
-  [state]
-  (distinct
-   (for [p (player-pieces state)
-         v (piece-vectors p)
-         :let [f (index-of state p)]
-         t (drop 1 (iterate (partial + v) f))
-         :let [m [f t]]
-         :while (legal-move? state m)]
-     m)))
+          (assoc-in [:grid from] (if (piece player-1-pieces) :x :X))
+          (assoc-in [:grid to] piece)))))
 
 (defn game-over?
   "Returns true if game is over."
   [state]
-  (let [moves (for [p (player-pieces state)
-                    v (piece-vectors p)
-                    :let [f (index-of state p)
-                          t (+ f v)]]
-                [f t])]
-    (not-any? #(legal-move? state %) moves)))
+  (= 0 (count (moves state))))
 
 (defn draw?
   "Returns true if the game is a draw."
   [state]
   (-> state
-      (update-in [:history] (fn [x] (conj x [0 0])))
-      (game-over?)))
+      (update-in [:history] (fn [x] (conj x :dummy)))
+      game-over?))
 
 (defn winner
   "Returns the winning player."
   [state]
-  (- 2 (-> state (:history) (count) (rem 2))))
+  (- 2 (-> state :history count (rem 2))))
 
 (defn- moves-left
   [s p] (str "  " (name p) ":" (moves-left? s p)))
@@ -153,8 +129,9 @@
 (defn to-string
   "Return a string representation of the board."
   [state]
-  (let [rows (->> state
-                  (:cells)
+  (let [grid (:grid state)
+        rows (->> locations
+                  (map #(get grid %))
                   (map (fn [x] (if (keyword? x) (name x) ".")))
                   (partition n-rows)
                   (map #(apply str %))
--- test/phage/core_test.clj
+++ test/phage/core_test.clj
@@ -5,30 +5,29 @@
 
 (deftest basics
   (testing "starting cells"
-    (is (= (-> start (:cells) (frequencies)) {nil 56
+    (is (= (-> start :grid vals frequencies) {nil 56
                                               :C 1 :S 1 :T 1 :D 1
                                               :c 1 :s 1 :t 1 :d 1}))
-    (is (= (occupied? start 0) :d))
-    (is (= (occupied? start (idx 7 7)) :D))
-    (is (nil? (occupied? start (idx 0 1))))
+    (is (= (occupied? start [0 0]) :d))
+    (is (= (occupied? start [7 7]) :D))
+    (is (nil? (occupied? start [0 1])))
 
     (is (= (moves-left? start :d) 7))
     (is (nil? (moves-left? start :x)))
-    (is (nil? (moves-left? start :X)))
-    (is (nil? (moves-left? start 0))))
+    (is (nil? (moves-left? start :X))))
 
   (testing "mobility"
-    (is (= (player-turn? start 0) :d))
-    (is (nil? (player-turn? start (idx 7 7))))
+    (is (= (player-turn? start [0 0]) :d))
+    (is (nil? (player-turn? start [7 7])))
 
-    (is (legal-move? start [0 1]))
-    (is (nil? (legal-move? start [0 (idx  1 1)])))
+    (is (legal-move? start [[0 0] [0 1]]))
+    (is (nil? (legal-move? start [[0 0] [1 1]])))
     (is (not (-> start
              (assoc-in [:moves-left :d] 0)
-             (legal-move? [0 1]))))
+             (legal-move? [[0 0] [0 1]]))))
 
-    (is (legal-move? start [(idx 1 2) (idx 2 2)]))
-    (is (nil? (legal-move? start [(idx 1 2) (idx 0 2)])))
+    (is (legal-move? start [[1 2] [2 2]]))
+    (is (nil? (legal-move? start [[1 2] [0 2]])))
 
     (is (= (count (moves start)) 61))
     (is (every? (partial legal-move? start) (moves start)))
@@ -37,32 +36,35 @@
 
 (deftest making-moves
   (testing "first move"
-    (let [s1 (move start [0 1])]
-      (is (= (-> s1 :cells frequencies) { nil 55 :x 1
-                                         :C 1 :S 1 :T 1 :D 1
-                                         :c 1 :s 1 :t 1 :d 1}))
-      (is (= [[0 1]] (-> s1 :history)))
+    (let [s1 (move start [[0 0] [0 1]])]
+      (is (= (-> s1 :grid vals frequencies) { nil 55 :x 1
+                                             :C 1 :S 1 :T 1 :D 1
+                                             :c 1 :s 1 :t 1 :d 1}))
+      (is (= [[[0 0] [0 1]]] (-> s1 :history)))
       (is (= 6 (-> s1 :moves-left :d)))
-      (is (= (occupied? s1 0) :x))
-      (is (= (occupied? s1 1) :d))
-      (is (not (legal-move? s1 [1 0])))
-      (is (not (legal-move? s1 [1 2]))))))
+      (is (= (occupied? s1 [0 0]) :x))
+      (is (= (occupied? s1 [0 1]) :d))
+      (is (not (legal-move? s1 [[0 1] [0 0]])))
+      (is (not (legal-move? s1 [[0 1] [0 2]]))))))
 
 (deftest game-over
   (testing "nowhere to go"
-    (let [cells (->> start (:cells) (replace {nil :x}))
-          s1 (assoc start :cells cells)]
+    (let [s0 start
+          s1 (assoc-in s0 [:grid] (zipmap (keys (:grid s0)) (repeat :x)))]
       (is (game-over? s1))
       (is (draw? s1))))
   (testing "no moves left"
-    (let [s1 (-> start (assoc :moves-left {}))]
+    (let [s0 start
+          s1 (assoc-in s0 [:moves-left] (zipmap (keys (:moves-left s0)) (repeat 0)))]
       (is (game-over? s1))
       (is (draw? s1))))
   (testing "not a draw?"
-    (let [s1 (-> start (assoc :moves-left {:T 1}))]
-      (is (game-over? s1))
-      (is (not (draw? s1)))
-      (is (= (winner s1) 2)))))
+    (let [s0 start
+          s1 (assoc-in s0 [:moves-left] (zipmap (keys (:moves-left s0)) (repeat 0)))
+          s2 (assoc-in s1 [:moves-left :T] 1)]
+      (is (game-over? s2))
+      (is (not (draw? s2)))
+      (is (= (winner s2) 2)))))
 
 (deftest stringification
   (testing "starting position"
@@ -84,7 +86,7 @@
 
   (testing "after 1 move"
     (is (= (-> start
-               (move [0 2])
+               (move [[0 0] [0 2]])
                (to-string))
            (str "7 .......D\n"
                 "6 .....T..\n"
@@ -103,8 +105,8 @@
 
   (testing "after 2 moves"
     (is (= (-> start
-               (move [0 2])
-               (move [(idx 4 1) (idx 4 4)])
+               (move [[0 0] [0 2]])
+               (move [[4 1] [4 4]])
                (to-string))
            (str "7 .......D\n"
                 "6 .....T..\n"
