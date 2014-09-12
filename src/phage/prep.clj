777b72d337fbbb17ace654c38eea022fd615a10b
Stig Brautaset <stig.brautaset@net-a-porter.com>
2014-09-12 19:42:30 +0100

    Initial tests of match state prep for frontende

---
+++ src/phage/prep.clj
@@ -0,0 +1,10 @@
+(ns phage.prep
+  (:require [phage.core :refer :all]))
+
+(defn prep
+  "Prepares a match for sending to the frontend client."
+  [state]
+  (-> state
+      (assoc-in [:game-over?] (game-over? state))
+      (assoc-in [:draw?] (draw? state))
+      (assoc-in [:winner] (winner state))))
---
+++ test/phage/prep_test.clj
@@ -0,0 +1,20 @@
+(ns phage.prep-test
+  (:require [phage.prep :refer :all]
+            [clojure.test :refer :all]
+            [phage.core :refer :all]))
+
+
+(deftest basics
+  (let [p (prep start)]
+    (is (not (:game-over? p)))
+    (is (= (-> p :grid vals) (-> start :grid vals)))
+    (is (= (-> p :history) (:history start)))
+    (is (= (-> p :moves-left) (:moves-left start)))))
+
+(deftest game-over
+  (let [m (loop [s start]
+            (if (game-over? s)
+              s
+              (recur (move s (rand-nth (moves s))))))
+        p (prep m)]
+    (is (true? (:game-over? p)))))
